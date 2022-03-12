bea4267ee2273e817b54669f2d7041f89cfd0138
zouritre <timouf97400@live.fr>
2022-03-12 12:27:01 +0100

    Reoganized base project structure and fixed unit test name

---
+++ CountOnMe/Model/VerifyExpressionValidity.swift
@@ -0,0 +1,14 @@
+//
+//  VerifyExpressionValidity.swift
+//  CountOnMe
+//
+//  Created by Bertrand Dalleau on 12/03/2022.
+//  Copyright © 2022 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class VerifyExpressionValidity {
+    
+    
+}
